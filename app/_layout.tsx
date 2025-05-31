62ac1f04731ef3633443ad36782b151afec345f7
Lukas <Lukas.Ledauskis@stud.smk.lt>
2025-05-31 23:45:56 +0300

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,27 @@
 import { Stack } from 'expo-router';
 import '../global.css';
 import { ClerkProvider } from '@clerk/clerk-expo';
-import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import Constants from 'expo-constants';
 
-const clerkApiKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const clerkApiKey = Constants.manifest?.extra?.clerkPublishableKey || Constants.expoConfig?.extra?.clerkPublishableKey;
+
+
+console.log('expoConfig:', Constants.expoConfig);
+console.log('manifest:', Constants.manifest);
+console.log('clerkApiKey:', clerkApiKey);
+
 
 if (!clerkApiKey) {
   throw new Error('Clerk API Raktas nerastas');
 }
 
-//console.log(clerkApiKey);
 
 export default function Layout() {
   return (
     <ClerkProvider publishableKey={clerkApiKey}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="gameScreen" options={{ headerShown: false }} />
-        <Stack.Screen name="optionsScreen" options={{ headerShown: false }} />
-        <Stack.Screen name="eulaScreen" options={{ headerShown: false }} />
-        <Stack.Screen name="profileScreen" options={{ headerShown: false }} />
-        <Stack.Screen name="mainPage" options={{ headerShown: false }} />
-
-        {/* Home(Index) */}
-        <Stack.Screen name="home" options={{ headerShown: false }} />
-        {/* Auth screen */}
-        <Stack.Screen name="(auth)/sign-in" options={{ headerShown: false }} />
+        {/* Add other screens as needed */}
       </Stack>
     </ClerkProvider>
   );
